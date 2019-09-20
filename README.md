18eb8396846136f4055ad302b0e0dd32f4696346
carluhsheyn <karlashanelaran@gmail.com>
2019-09-20 15:47:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pvqls1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pvqls1)
---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { FlickrService } from './flickr.service';
+import { FormControl } from '@angular/forms';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-flickr-photo-search';
+
+  public inputList : any = [];
+  public pageNumber : number = 0;
+  public inputQuery = new FormControl('');
+  public inputResult: string = null;
+  public totalResults = null;
+  public selectedPhoto = null;
+  public isLoading : boolean = false;
+  public inputRequired: string = "";
+  public totalPage = null;
+
+  constructor(private _flickrService : FlickrService) { }
+
+  pagination() {
+    this.isLoading = true;
+    this._flickrService.getPhoto(this.pageNumber, this.inputQuery.value).subscribe(result => {
+      this.inputList = result.photos.photo;
+      this.isLoading = false;
+    })
+  }
+
+  searchQuery() {
+    if (this.inputQuery.value == "") {
+      this.inputRequired = "Input Required";
+    } else {
+      this.isLoading = true;
+      this._flickrService.getPhoto(this.pageNumber, this.inputQuery.value).subscribe(result => {
+        this.inputList = result.photos.photo;
+        this.inputResult = this.inputQuery.value;
+        this.totalResults = result.photos.total;
+        this.isLoading = false;
+        this.pageNumber = 1;
+        this.totalPage = result.photos.pages;
+        this.inputRequired = "";
+      })
+    }
+  }
+
+  photoInfo(id : number) {
+    this._flickrService.getPhotoInfo(id).subscribe(result => {
+      this.selectedPhoto = result.photo;
+      $('#photoModal').modal('show');
+    })
+  }
+
+  showPhoto() {
+    return this._flickrService.displayPhoto(this.selectedPhoto);
+  }
+
+  getPhotoUrl(input : any) : string {
+    return this._flickrService.displayPhoto(input);
+  }
+}
